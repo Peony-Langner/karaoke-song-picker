50e957be104aaecb41fdd761b762383bb9639c79
Peony Langner <peony.langner@hotmail.de>
2022-03-30 18:11:56 +0200

    Added rockSong route/controller

--- controller/songListController.js
+++ controller/songListController.js
@@ -1,5 +1,33 @@
 const SongList = require('./../models/SongList');
 
+exports.addRockSong = async (req, res) => {
+  const { body } = req;
+
+  try {
+    const newSong = await SongList.create({
+      artist: body.artist,
+      title: body.title,
+      genre: body.genre,
+    });
+
+    return res.status(200).json({ message: 'Song Created', newSong });
+  } catch (error) {
+    return res.status(400).json({ message: 'Error happened', error });
+  }
+};
+
+exports.listRockSongs = async (req, res) => {
+  try {
+    const listRockSongs = await SongList.find({ genre: 'Rock' });
+
+    return res
+      .status(200)
+      .json({ message: 'list of rocksongs', listRockSongs });
+  } catch (error) {
+    return res.status(400).json({ message: 'Error happened' });
+  }
+};
+
 exports.addSong = async (req, res) => {
   const { body } = req;
 
--- routes/songListRoute.js
+++ routes/songListRoute.js
@@ -4,6 +4,12 @@ const controller = require('../controller/songListController');
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Welcome!'));
+
+//* dummy lists:
+router.get('/listrocksongs', controller.listRockSongs);
+router.post('/addrocksong', controller.addRockSong);
+
+//* personalized list
 router.get('/list', controller.listSongs);
 router.post('/add', controller.addSong);
 
